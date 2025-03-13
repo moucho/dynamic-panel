573752e64949cb9f3d5d2798e8d3b01ce2474eeb
velade <velade48@gmail.com>
2025-03-13 14:13:49 +0800

    添加对.gitignore的排除

--- pack.sh
+++ pack.sh
@@ -10,6 +10,6 @@ zip_file="dynamic-panel@velhlkj.com_v${current_version}.zip"
 exclude_files="*.md *.txt *.po *.pot *.zip locale/compile_all.sh pack.sh"
 
 # 打包当前目录，排除指定文件
-zip -r "$zip_file" . -x "./readme_images/*" "./.git/*" $exclude_files
+zip -r "$zip_file" . -x "./readme_images/*" "./.git/*" ".gitignore" $exclude_files
 
 echo "已打包为: $zip_file"
